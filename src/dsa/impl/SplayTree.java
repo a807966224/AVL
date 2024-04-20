50003908a12c8a1432b6cb113b765606bba550d7
爱因斯唐 <1745525+einsitang@users.noreply.github.com>
2024-04-20 12:14:00 +0000

    splaytree only with inner insert contains remove

--- src/dsa/impl/SplayTree.java
+++ src/dsa/impl/SplayTree.java
@@ -1,26 +1,33 @@
 package dsa.impl;
 
 import dsa.iface.IPosition;
+import dsa.inner.InnerSplayTree;
 
 public class SplayTree<T extends Comparable<T>> extends BinarySearchTree<T> {
 
-   @Override
-   public void insert( T value ) {
-      // TODO: Implement the insert(...) method.
+   private InnerSplayTree<T> ist;
+
+   public SplayTree() {
+      ist = new InnerSplayTree<T>();
    }
 
    @Override
-   public boolean contains( T value ) {
-      // TODO: Implement the contains(...) method.
-      return false;
+   public void insert(T value) {
+      ist.insert(value);
    }
 
    @Override
-   public void remove( T value ) {
-      // TODO: Implement the remove(...) method.
+   public boolean contains(T value) {
+      return ist.searchTree(value) != null;
    }
 
-   private void splay( IPosition<T> n ) {
-      // TODO: Implement the splay(...) method.
+   @Override
+   public void remove(T value) {
+      ist.deleteNode(value);
+   }
+
+   private void splay(IPosition<T> n) {
+      // do nothing because auto splay
+      
    }
 }
